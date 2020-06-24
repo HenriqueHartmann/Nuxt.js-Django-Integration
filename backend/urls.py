e3eff0aa6861dcb1e630018dc5b7177fc5cff2fb
Hartmann <alemaohartmann@gmail.com>
2020-06-24 14:38:40 -0300

    :taxi: Defining the routes

--- backend/urls.py
+++ backend/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+from backend.core.router import router
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/token/', TokenObtainPairView.as_view(), name='token'),
-    path('api/refresh_token/', TokenRefreshView.as_view(), name='refresh_token'),
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token"),
+    path("api/refresh_token/", TokenRefreshView.as_view(), name="refresh_token"),
 ]
